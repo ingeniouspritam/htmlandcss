ada0821571e9df234843104f4bb3f9819abd2d04
Abhinav <ingeniousabhinav@gmail.com>
2022-10-03 00:12:34 +0530

    Some changes in index file and added css and Js Files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// ######## javascript 
+//     Defination - an object-oriented computer programming language commonly used to create interactive effects within web browsers.
+
+function Alert(){
+    alert("I am an Alert !");
+    console.log("Alert Works");
+  }
